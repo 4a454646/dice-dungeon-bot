6ef17c78d6b8aa013177a63e847daf5130568bf0
4a454646 <46868596+4a454646@users.noreply.github.com>
2019-06-28 09:28:17 -0700

    Add files via upload

---
+++ example_dice_config.py
@@ -0,0 +1,10 @@
+database_config = {'host': 'host (ip address if remote, localhost otherwise)', 
+                    'db': 'database_name', 
+                    'user': 'username', 
+                    'password': 'password', 
+                    "autocommit": True}
+#set this up so that you are able to log in and connect to your database
+#the user should have enough permissions to read and write data
+bot_token = "NTcyNjMwNjk1ODM1MzM2NzA2.XRY_Ww.6rDhQ99ezwJHh2ldjF5CDC-kXgA"
+#your bot's token goes here
+#note that the token I included here is an improper token, so don't worry
